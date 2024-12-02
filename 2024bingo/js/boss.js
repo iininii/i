67d802a750c72b57e9dd6389a407cbb73b996b94
rico <a0955383566@gmail.com>
2024-12-03 00:02:26 +0800

    feat: fetch boss team paths

--- 2024bingo/js/boss.js
+++ 2024bingo/js/boss.js
@@ -10,7 +10,26 @@ function init() {
             return;
         }
         fetchTeamPaths().then(result => {
-
+            if (result.teamPaths) {
+                result.teamPaths.forEach(teamPath => {
+                    const div = document.getElementById("path-radio-div");
+                    const radioButton = document.createElement("input");
+                    radioButton.type = "radio";
+                    radioButton.name = "team-answer";
+                    radioButton.className = "team-line-item";
+                    radioButton.id = teamPath[0];
+                    const label = document.createElement("label");
+                    label.setAttribute("for", teamPath[0]);
+                    const labelTextElement = document.createElement("p");
+                    labelTextElement.innerHTML = teamPath[1];
+                    const image = document.createElement("img");
+                    image.src = 'images/' + teamPath[2] + '.webp' ;
+                    label.appendChild(labelTextElement);
+                    label.appendChild(image);
+                    div.appendChild(radioButton);
+                    div.appendChild(label);
+                })
+            }
         })
         gamingPage();
         dateCountdown(result.startTime);
